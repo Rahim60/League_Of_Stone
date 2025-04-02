0416c9683f51aedd1fa98a94713a2811ca7f2252
Rahim60 <djoboram02@gmail.com>
2025-04-02 23:42:15 +0200

    Update jeu.js

--- los/src/pages/game/jeu.js
+++ los/src/pages/game/jeu.js
@@ -1,8 +1,155 @@
-import React from 'react';
-import Plateau from '@/components/plateau';
+import { useEffect } from "react";
+import Plateau from "@/components/Plateau";
 
-export default function jeu(){
+const API = "http://localhost:3001/match";
 
+export default function Jeu(){
+    const [token, setToken] = useState("");
+    const [deck, setDeck] = useState([]);
+    const [hp, setHp] = useState(null);
+    const [hand, setHand] = useState([]);
+    const [board, setBoard] = useState([]);
+    const [cardPicked, setCardPicked] = useState(false);
+    const [turn, setTurn] = useState(false);
+
+    useEffect(() => {
+        const storedDeck = sessionStorage.getItem("deck");
+        const storedToken = sessionStorage.getItem("token");
+        if (!storedDeck || !storedToken) router.push("/signin");
+        if (storedDeck){ 
+            setToken(storedToken);
+            initDeck(storedToken);
+            
+        }
+    },[]);
+    const getInfo = () =>{
+        fetch(`${API}/getMatch`,{
+            headers:{
+                "Content-Type":"application/json",
+                "www-autenticate":token
+            }
+        })
+        .then(response =>{
+            if (!response.ok) throw new Error ("Erreur lors de la récupération des infos");
+            return response.json();
+        })
+        .then(rep =>{
+            console.log("Infos du match : ",rep);
+            setHp(rep.hp);
+            setHand(rep.hand);
+            setBoard(rep.board);
+            setCardPicked(rep.cardPicked);
+            setTurn(rep.turn);
+        })
+        .catch(err => console.error("Erreur : ",err))
+    };
+    const initDeck = (toke) =>{
+        fetch(`${API}/initDeck?deck=${storedDeck}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "www-authenticate": toke
+            }
+        })
+        .then(response =>{
+            if (!response.ok) throw new Error ("Erreur lors de l'initialisation du deck");
+            return response.json();
+        })
+        .then(rep => {
+            console.log("Deck initialisé : ",rep);
+            getInfo();
+        })
+        .catch(err =>{
+            console.error("Erreur : ",err);
+        })
+    };
+    const pioche = ()=>{
+        fetch(`${API}/pickCard`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "www-authenticate": token
+            }
+        })
+        .then(response =>{
+            if (!response.ok) throw new Error ("Erreur lors de la pioche");
+            return response.json();
+        })
+        .then(rep =>{
+            console.log("Pioche : ",rep);
+            getInfo();
+        })
+        .catch(err =>{
+            console.error("Erreur : ",err);
+        })
+    };
+    const playCard = (cardKey) =>{
+        fetch(`${API}/playCard?card=${cardKey}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "www-authenticate": token
+            }
+        })
+        .then(response =>{
+            if (!response.ok) throw new Error ("Erreur lors de la pose de la carte");
+            return response.json();
+        })
+        .then(rep =>{
+            console.log("Carte posée");
+            setBoard(rep.board);
+            setHand(rep.hand);
+        })
+        .catch(err =>console.error("Erreur : ",err))
+    };
+    const attackCard = (cardKey, targetCardKey) =>{
+        fetch(`${API}/attack?card=${cardKey}&ennemyCard=${targetCardKey}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "www-authenticate": token
+            }
+        })
+        .then(response =>{
+            if (!response.ok) throw new Error ("Erreur lors de l'attaque");
+            return response.json();
+        })
+        .then(rep =>console.log("Attaque réalisée : ",rep))
+        .catch(err =>console.error("Erreur : ",err));
+    }
+
+    const attackPlayer = (cardKey) =>{
+        fetch(`${API}/attackPlayer?card=${cardKey}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "www-authenticate": token
+            }
+        })
+        .then(response =>{
+            if (!response.ok) throw new Error ("Erreur lors de l'attaque du joueur");
+            return response.json();
+        })
+        .then(rep =>console.log("Attaque du joueur réalisée: ",rep))
+        .catch(err =>console.error("Erreur : ",err));
+    }
+    const endTurn = () =>{
+        fetch(`${API}/endTurn`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "www-authenticate": token
+            }
+        })
+        .then(response =>{
+            if (!response.ok) throw new Error ("Erreur lors de la fin de tour");
+            return response.json();
+        })
+        .then(rep =>{
+            console.log("Fin de tour : ",rep)
+            getInfo();
+        })
+        .catch(err =>console.error("Erreur : ",err));
+    }
     return(
         <>
             <Plateau />
