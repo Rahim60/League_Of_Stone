5ca9c11b787cfc4e1c54213fbe4013c8394c8806
Rahim60 <djoboram02@gmail.com>
2025-04-03 15:00:24 +0200

    axios

---
+++ los/src/utils/axios-config.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3001";
+
+axios.interceptors.request.use(
+    (config) => {
+        const excludedPaths = [`/user`, `/login`, `/cards`];
+        const isExcluded = excludedPaths.some((path) => config.url.includes(path));
+
+        if (!isExcluded) {
+            const token = sessionStorage.getItem("token");
+            if (token) config.headers["WWW-Authenticate"] = token;
+        }
+
+        return config;
+    }, (error) => (Promise.reject(error))
+)
